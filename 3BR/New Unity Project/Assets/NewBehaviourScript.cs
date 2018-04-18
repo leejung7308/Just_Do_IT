092e632cc925dd76a11b617d5be31c152c77410a
w4ei756fg <35715109+w4ei756fg@users.noreply.github.com>
2018-04-18 15:49:33 +0900

    3BR Source

---
+++ 3BR/New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0.0f, -1.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0.0f, 1.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-0.05f, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0.05f, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0.0f, 0.0f, 0.1f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0.0f, 0.0f, -0.1f);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(0.0f, 3.0f, 0.0f);
+        }
+    }
+}

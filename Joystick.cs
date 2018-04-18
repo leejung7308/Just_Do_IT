1bb3f5f26fe3f16c28cfc7820a573aa1bcd93218
leejung7308 <leekkzzzz@naver.com>
2018-04-18 15:37:36 +0900

    Add files via upload

---
+++ Joystick.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    private Image bgImg;
+    private Image joystickImg;
+    private Vector3 inputVector;
+
+	void Start ()
+    {
+        bgImg = GetComponent<Image>();
+        joystickImg = transform.GetChild(0).GetComponent<Image>();
+		
+	}
+
+    public virtual void OnDrag(PointerEventData ped)
+    {
+        Debug.Log("Joystick >>> OnDrag()");
+
+        Vector2 pos;
+        if(RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        {
+            pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / bgImg.rectTransform.sizeDelta.y);
+
+            inputVector = new Vector3(pos.x * 2 + 1, pos.y * 2 - 1, 0);
+            inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
+
+            //Move Joystick IMG
+            joystickImg.rectTransform.anchoredPosition = new Vector3(inputVector.x * (bgImg.rectTransform.sizeDelta.x / 3)
+                                                                   , inputVector.y * (bgImg.rectTransform.sizeDelta.y / 3));
+        }
+    }
+    
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+        OnDrag(ped);
+    }
+
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+        inputVector = Vector3.zero;
+        joystickImg.rectTransform.anchoredPosition = Vector3.zero;
+    }
+
+    //나중에 쓸거
+    public float GetHorizontalValue()
+    {
+        return inputVector.x;
+    }
+
+    public float GetVerticalValue()
+    {
+        return inputVector.y;
+    }
+
+	void Update () {
+		
+	}
+}
